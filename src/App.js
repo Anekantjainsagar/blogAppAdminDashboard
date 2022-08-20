35e02f00aec2962ce50855c9368346f674e4390d
Anekant <Anekantjainsagar@gmail.com>
2022-08-20 21:57:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import Navbar from "./Components/Navbar/index";
+import Footer from "./Components/Footer/index";
+import MainScreen from "./Screens/MainScreen/index";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <MainScreen />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Screens/MainScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const MainScreen = () => {
+  return (
+    <div>MainScreen</div>
+  )
+}
+
+export default MainScreen
